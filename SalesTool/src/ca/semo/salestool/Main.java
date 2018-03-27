3fd60a9c7bcea035a42ab1aadad6b6c1ef6ca9ab
nilesh.ingawale <nilesh.ingawale@NIT-LPT-0107.nitorinfotech.net>
2018-03-27 21:20:44 +0530

    commit

---
+++ SalesTool/src/ca/semo/salestool/Main.java
@@ -0,0 +1,17 @@
+package ca.semo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		SalesData data = new SalesData();
+		DisplayGreetings();
+		data.Display();
+
+	}
+
+	private static void DisplayGreetings()
+	{
+		System.out.println("Hello people");
+	}
+}
---
+++ SalesTool/src/ca/semo/salestool/SalesData.java
@@ -0,0 +1,11 @@
+package ca.semo.salestool;
+
+public class SalesData 
+{
+
+
+	public void Display()
+	{
+		System.out.println(" in Display");
+	}
+}
